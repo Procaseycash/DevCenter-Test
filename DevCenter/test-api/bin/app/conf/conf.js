5676d97e7e05feeb19ffb5b485349721134a99b2
KazeemO <kezyolanipekun@gmail.com>
2018-02-24 01:07:38 +0100

    Test 11

--- DevCenter/test-api/bin/app/conf/conf.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GIPHY_HOST = 'http://api.giphy.com/';
-const GIPHY_API_KEY = 'frtNsOD0ZrjfSUyFxYcO5ULYKOXFCZew';
-exports.GIPHY_PATH = 'v1/gifs/translate?api_key=' + GIPHY_API_KEY + '&s=';
-//# sourceMappingURL=conf.js.map
